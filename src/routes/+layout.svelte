51ca98a0c74422921a350bac64b0785a5c00ef21
Andrew Glyadchenko <andrewgly95@gmail.com>
2024-04-13 10:38:28 -0400

    Update responsive styles in app.scss and layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,49 @@
 <script>
 	import '../app.scss';
-</script>
+  let showNavOptions = true;
 
+  function toggleNavOptions() {
+    showNavOptions = !showNavOptions;
+  }
+</script>
+<main class="main">
+	<header>
+		<div class="logo" on:click="{toggleNavOptions}">
+			<img src="wordmark.png" alt="Andrew Glyadchenko" />
+		</div>
+ 
+		<nav class:active="{showNavOptions}">
+			<ul class="links">
+				<li><a href="#about">About</a></li>
+				<li><a href="#skills">Skills</a></li>
+				<li>
+					<a href="#experience">Experience</a>
+				</li>
+				<li>
+					<a href="#projects">Projects</a>
+				</li>
+				<li><a href="#contact">Contact</a></li>
+			</ul>
+		</nav>
+	</header>
 <slot />
+
+	<footer>
+		<p>&copy; 2024 Andrew Glyadchenko. All rights reserved.</p>
+		<ul class="social-links">
+			<li>
+				<a href="https://www.linkedin.com/in/andrew-glyadchenko-13b85498/" target="_blank"
+					><img src="linkedin-mark.png" /></a
+				>
+			</li>
+			<li>
+				<a href="https://github.com/andgly95" target="_blank"
+					><img src="github-mark.png" /></a
+				>
+			</li>
+			<li>
+				<a href="https://twitter.com/andgly95" target="_blank"><span>𝕏</span></a>
+			</li>
+		</ul>
+	</footer>
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,4 @@
-<main class="main">
-	<header>
-		<div class="logo">
-			<img src="wordmark.png" alt="Andrew Glyadchenko" />
-		</div>
-		<nav>
-			<ul class="links">
-				<li><a href="#about">About</a></li>
-				<li><a href="#skills">Skills</a></li>
-				<li>
-					<a href="#experience">Experience</a>
-				</li>
-				<li>
-					<a href="#projects">Projects</a>
-				</li>
-				<li><a href="#contact">Contact</a></li>
-			</ul>
-		</nav>
-	</header>
+
 	<section class="hero">
 		<div class="left">
 			<h1 class="name">Hi, I'm Andrew Glyadchenko</h1>
@@ -170,22 +152,3 @@
 			<button type="submit">Send Message</button>
 		</form>
 	</section>
-	<footer>
-		<p>&copy; 2024 Andrew Glyadchenko. All rights reserved.</p>
-		<ul class="social-links">
-			<li>
-				<a href="https://www.linkedin.com/in/andrew-glyadchenko-13b85498/" target="_blank"
-					><img src="linkedin-mark.png" /></a
-				>
-			</li>
-			<li>
-				<a href="https://github.com/andgly95" target="_blank"
-					><img src="github-mark.png" /></a
-				>
-			</li>
-			<li>
-				<a href="https://twitter.com/andgly95" target="_blank"><span>𝕏</span></a>
-			</li>
-		</ul>
-	</footer>
-</main>
